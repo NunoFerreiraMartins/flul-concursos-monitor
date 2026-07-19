fd12da7dae97c048136a9433b483859bcc860b67
NunoFerreiraMartins <40354953+NunoFerreiraMartins@users.noreply.github.com>
2026-07-19 23:40:11 +0100

    Update check.py

--- check.py
+++ check.py
@@ -1,45 +1,41 @@
-"""
-check.py
-
-Programa principal.
-"""
-
+import os
 import traceback
 
 from parser import extrair_concursos
 from storage import atualizar, novos
 from mail import enviar_email
+from config import DATABASE_FILE
 
 
 def main():
 
-    print("=" * 60)
-    print("Monitor FLUL")
-    print("=" * 60)
-
     concursos = extrair_concursos()
 
-    print(f"Foram encontrados {len(concursos)} concursos na página.")
+    print(f"{len(concursos)} concursos encontrados.")
 
-    if not concursos:
-        print("Nenhum concurso encontrado.")
+    if not os.path.exists(DATABASE_FILE):
+
+        print("Primeira execução.")
+
+        atualizar(concursos)
+
+        print("Histórico criado. Não será enviado qualquer email.")
+
         return
 
     concursos_novos = novos(concursos)
 
     if concursos_novos:
 
-        print(f"Foram encontrados {len(concursos_novos)} concursos novos.")
-
         enviar_email(concursos_novos)
+
+        print(f"{len(concursos_novos)} novos concursos enviados.")
 
     else:
 
-        print("Não existem concursos novos.")
+        print("Sem novidades.")
 
     atualizar(concursos)
-
-    print("Histórico atualizado.")
 
 
 if __name__ == "__main__":
@@ -48,13 +44,5 @@ if __name__ == "__main__":
         main()
 
     except Exception:
-
-        print()
-
-        print("=" * 60)
-        print("ERRO")
-        print("=" * 60)
-
         traceback.print_exc()
-
         raise
